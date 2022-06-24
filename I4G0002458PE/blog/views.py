48441b5abb820786494591474ac32b5208e60106
zechariah popoola <olabode.oritogun@gmail.com>
2022-06-24 16:16:18 +0100

    updated changes

---
+++ I4G0002458PE/blog/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.views.generic import ListView,DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from blog.models import Post
+
+# Create your views here.
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url: reverse_lazy("blog:all")
+    template_name= "blog/post_form.html"
+
+
+class PostDetailView(DeleteView):
+    model = Post
+    template_name = "blog/post_detail.html"
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+    template_name = "blog/post_form.html"
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url: reverse_lazy("blog:all")
+    template_name = "blog/post_confirm_delete.html"
